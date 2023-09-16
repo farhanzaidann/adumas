0f454437f4886607d8dd4ca120b4c4bd2c1fa0d0
farhanzaidann <farhnzaidan@gmail.com>
2023-09-17 02:23:55 +0700

    add env from kb

--- lib/core/env.dart
+++ lib/core/env.dart
@@ -1,4 +1,25 @@
 // LOCAL IP TO CONNECT ON MONGODB
 
+import 'package:dio/dio.dart';
+
 String uri = 'http://192.168.1.7:5000';
 String mdApi = 'https://us-east-1.aws.data.mongodb-api.com/app/data-rvnlm/endpoint/data/v1';
+
+// KONTENBASE API
+
+String kbApi = 'https://api.kontenbase.com/query/api/v1/a9fc3e08-8fce-458b-9b8a-7b8b5330ed0f';
+
+Future<String> getSysToken() async {
+  Dio dio = Dio();
+  try {
+    Response res = await dio.post("$kbApi/auth/login",
+        data: {"email": "token@token.token", "password": "2wsx1qaz"});
+    if (res.statusCode == 200) {
+      return res.data["token"];
+    }
+  } catch (e) {
+    print("error token $e");
+    return "ERROR";
+  }
+  return "ERROR";
+}
